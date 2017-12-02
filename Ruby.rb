b807cf1528d86ec523f35fbba577348b1bbd9d29
Ana Frida <anafrida@gmail.com>
2017-12-02 09:49:48 -0200

    Criar o arquivo de loop

---
+++ Ruby.rb
@@ -0,0 +1,17 @@
+
+val_boolean = false
+val_string = "Frida"
+
+val_num = 0
+loop do
+  val_num += 1
+  puts "TESTE NUMERO: #{val_num}"
+  break if val_num == 80
+  
+end
+
+
+puts "TESTE BOOLEANO: #{val_boolean}"
+
+puts "TESTE STRING: #{val_string}"
+
