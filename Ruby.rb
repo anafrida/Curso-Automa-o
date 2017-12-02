cf75098ae982eea50181e3f6235212e84f856f1d
Ana Frida <anafrida@gmail.com>
2017-12-02 10:08:40 -0200

    Atualização do Código

--- Ruby.rb
+++ Ruby.rb
@@ -6,12 +6,11 @@ val_num = 0
 loop do
   val_num += 1
   puts "TESTE NUMERO: #{val_num}"
+  puts "TESTE STRING: #{val_string}"
+
   break if val_num == 80
   
 end
-
-
 puts "TESTE BOOLEANO: #{val_boolean}"
-
 puts "TESTE STRING: #{val_string}"
 
